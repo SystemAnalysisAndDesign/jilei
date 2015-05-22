6bf72dd6370da56079a8bc1e9c2c81a6044e253a
renbaoke <1193443688@qq.com>
2015-05-22 12:04:35 +0800

    分离header，添加universities

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 /*
  * 网站首页
  * 数据由controller/index.control.php生成，由template/index.html.php表现
- * 数据约定：
+ * 数据格式：
+ * $universities = array(
+ * 0 => array("name" => "SYSU"),
+ * ...
+ * )
  * 
+ * $items = array(
+ * 0 => array("id" => 0, "title" => "Title", "university" => 0, "timestamp" => 0),
+ * ...
+ * )
  */
 
 $config = require 'config.php';
+$universities = require 'university.php';
 
 if ($config["TEST"] === TRUE)
 	require 'test/index.test.php';
---
+++ universities.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+		0 => array("name" => "中山大学"),
+		1 => array("name" => "广东外语外贸大学")
+);
