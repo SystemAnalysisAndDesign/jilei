8c39b0c41a65052d793a483221494a752be96e9b
renbaoke <1193443688@qq.com>
2015-05-27 22:01:40 +0800

    将格式化进行到底

--- universities.php
+++ universities.php
@@ -2,7 +2,11 @@
 /*
  * 大学信息数据，极少变动，储存于此
  */
-return array(
-		0 => array("name" => "中山大学"),
-		1 => array("name" => "广东外语外贸大学")
+return array (
+		0 => array (
+				"name" => "中山大学" 
+		),
+		1 => array (
+				"name" => "广东外语外贸大学" 
+		) 
 );
